e348cd838855764f694616c2ef8ad421ce4385c6
renziNNN <renziNNN1337@gmail.com>
2019-09-04 20:59:15 +0200

    MeteorRain started

--- RGBWiFi/RGB_LED_ESP8266/Strip.h
+++ RGBWiFi/RGB_LED_ESP8266/Strip.h
@@ -96,7 +96,7 @@ class Strip
             adafruitStrip.show();
             break;
           case METEOR:
-            // TODO: implement
+            meteorRain(10, 64, true, 30);
             adafruitStrip.show();
             break;
           
@@ -121,6 +121,56 @@ class Strip
     		adafruitStrip.setPixelColor(i, (ij -170) * 3, 255 - (ij -170) * 3, 0);
     	}
       adafruitStrip.show();
+    }
+
+    void meteorRain(byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) 
+    {  
+      setAll(0,0,0);
+      
+      for(int i = 0; i < NUM_LEDS+NUM_LEDS; i++) {
+        
+        
+        // fade brightness all LEDs one step
+        for(int j=0; j<NUM_LEDS; j++) {
+          if( (!meteorRandomDecay) || (random(10)>5) ) {
+            fadeToBlack(j, meteorTrailDecay );        
+          }
+        }
+        
+        // draw meteor
+        for(int j = 0; j < meteorSize; j++) {
+          if( ( i-j <NUM_LEDS) && (i-j>=0) ) {
+            setPixel(i-j, red, green, blue);
+          } 
+        }
+       
+        adafruitStrip.show()
+        delay(SpeedDelay);
+      }
+    }
+    
+    void fadeToBlack(int ledNo, byte fadeValue) {
+     #ifdef ADAFRUIT_NEOPIXEL_H 
+        // NeoPixel
+        uint32_t oldColor;
+        uint8_t r, g, b;
+        int value;
+        
+        oldColor = adafruitStrip.getPixelColor(ledNo);
+        r = (oldColor & 0x00ff0000UL) >> 16;
+        g = (oldColor & 0x0000ff00UL) >> 8;
+        b = (oldColor & 0x000000ffUL);
+    
+        r=(r<=10)? 0 : (int) r-(r*fadeValue/256);
+        g=(g<=10)? 0 : (int) g-(g*fadeValue/256);
+        b=(b<=10)? 0 : (int) b-(b*fadeValue/256);
+        
+        adafruitStrip.setPixelColor(ledNo, r,g,b);
+     #endif
+     #ifndef ADAFRUIT_NEOPIXEL_H
+       // FastLED
+       leds[ledNo].fadeToBlackBy(fadeValue);
+     #endif  
     }
 
     // ------------------------------------------------------------------------------------------ SET CONFIG FUNCTIONS
