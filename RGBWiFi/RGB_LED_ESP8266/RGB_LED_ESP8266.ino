ccca0646cf6328fb4dbd4d46aba3e1e021b507f1
Barabási Richárd <barabasi.richard@gmail.com>
2019-09-07 16:05:33 +0200

    Bugfix, feature addition Fixed bug crashing CPU on WiFi connection loss Added new console commands
    Signed-off-by: Barabási Richárd <barabasi.richard@gmail.com>

--- RGBWiFi/RGB_LED_ESP8266/RGB_LED_ESP8266.ino
+++ RGBWiFi/RGB_LED_ESP8266/RGB_LED_ESP8266.ino
@@ -18,13 +18,9 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 60000);     // protoco
 // requires support on windows 10 which is not state of the art yet...
 //#define MDNS_ADDRESS "ESPI"
 
-// Verbose almost debug like mode
 //#define VERBOSE_MODE
 
 #define HEARTBEAT_PERIOD 120000 // in ms
-#ifdef HEARTBEAT_PERIOD
-  uint32_t timer = HEARTBEAT_PERIOD;
-#endif
 
 // EEPROM
 #define RETAIN_COMMAND_FOR              86400   // in seconds
@@ -37,13 +33,21 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 60000);     // protoco
 
 // ------------------------------------------------------------------------------------------ VARIABLES
 uint32_t lastCommandEpochTick, seedGlobal;
+uint32_t timer = HEARTBEAT_PERIOD;
 
 bool isCommandValid;
 
 // ------------------------------------------------------------------------------------------ IGNORED PART
-/* // Template contet for "IgnoredDefines.h"
+// Template contet for "IgnoredDefines.h"
+/* 
+ * #define NTP_UPDATE
+ *  
  * #define WIFI_SSID "---"
  * #define WIFI_PW  "---"
+ * 
+ * IPAddress staticIP(192,168,1,100);
+ * IPAddress gateway(192,168,0,0);
+ * IPAddress subnet(255,255,255,0);
  * 
  * // strips settings
  * #define NUMBER_OF_STRIPS                3
@@ -68,19 +72,21 @@ void setup(void)
 {
   // ------------------------------ SERIAL
   Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PW);
-  Serial.println("");
 
   // ------------------------------ WIFI
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.println("WIFI CONNECTING...");
+  WiFi.setAutoReconnect(false);
+  //TODO: implement sleep mode while we have trees left...
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  WiFi.config(staticIP, gateway, subnet);
+  WiFi.begin(WIFI_SSID, WIFI_PW);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP address is ");
-  Serial.println(WiFi.localIP());
+  getWifiInfo();
 
   // ------------------------------ MDNS
   #ifdef MDNS_ADDRESS
@@ -97,6 +103,7 @@ void setup(void)
   // ------------------------------ HANDLES
   server.onNotFound(handleNotFound);
   server.on("/command", handleCommand);
+  server.on("/ota", OTAModeOn);
 
   // ------------------------------ OTA
   ArduinoOTA.onStart([]() 
@@ -113,9 +120,7 @@ void setup(void)
     EEPROM.end();
     SPIFFS.end();
     timeClient.end();
-    #ifdef VERBOSE_MODE
-      Serial.println("Start updating " + type);
-    #endif
+    Serial.println("Start updating " + type);
   });
 
   // These are not used at the time, but there is the opportunity
@@ -157,13 +162,10 @@ void setup(void)
   */
   
   // ------------------------------ BEGINS
-  ArduinoOTA.begin();
+  ArduinoOTA.begin(false);  // false to begin without mDNS support
   SPIFFS.begin(); 
-  EEPROM.begin(64);   //start eeprom with 64byte
+  EEPROM.begin(64);         //start eeprom with 64byte
   server.begin();
-  #ifdef VERBOSE_MODE
-      Serial.println("HTTP server started");
-  #endif
   timeClient.begin();
   timeClient.forceUpdate();
 
@@ -198,7 +200,8 @@ void loop(void)
   #endif
 
   server.handleClient();
-  
+
+  // TODO: decide to keep this or OTA mode
   ArduinoOTA.handle();
   
   seedGlobal = micros();
@@ -213,15 +216,18 @@ void loop(void)
   {
     uint8_t command = Serial.read();
     if (command == (uint8_t)'r')
+    {
+      Serial.println("Restart initiated! Restarting in 2 sec!");
+      delay(2000);
       ESP.restart();
+    }
     else if (command == (uint8_t)'m')
-      Serial.println(system_get_free_heap_size());
+    {
+     getMemoryInfo();
+    }
     else if (command == (uint8_t)'t')
     {
-      Serial.print("Ellapsed minutes since boot: ");
-      Serial.println(millis()/60000);
-      Serial.print("Time from NTP server: ");
-      Serial.println(timeClient.getFormattedTime());
+      getTimeInfo();
     }
     else if (command == (uint8_t)'u')
     {
@@ -238,42 +244,219 @@ void loop(void)
       for(uint8_t index = 0; index < NUMBER_OF_STRIPS; index++)
       {
         strips[index].setEffect(0);
+        Serial.print("Strip number ");
+        Serial.print(index);
+        Serial.print(" is set to OFF");
       }
     }
-    
+    else if (command == (uint8_t)'n')
+    {
+      Serial.print("Wifi status is: ");
+      switch(WiFi.status())
+      {
+        case WL_CONNECTED:
+          Serial.println("WL_CONNECTED");
+          getWifiInfo();
+          break;
+        case WL_NO_SHIELD:
+          Serial.println("WL_NO_SHIELD");
+          break;
+        case WL_IDLE_STATUS:
+          Serial.println("WL_IDLE_STATUS");
+          break;
+        case WL_NO_SSID_AVAIL:
+          Serial.println("WL_NO_SSID_AVAIL");
+          break;
+        case WL_SCAN_COMPLETED:
+          Serial.println("WL_SCAN_COMPLETED");
+          break;
+        case WL_CONNECT_FAILED:
+          Serial.println("WL_CONNECT_FAILED");
+          break;
+        case WL_CONNECTION_LOST:
+          Serial.println("WL_CONNECTION_LOST");
+          break;
+        case WL_DISCONNECTED:
+          Serial.println("WL_DISCONNECTED");
+          break;
+        default:
+          Serial.println(WiFi.status());
+          Serial.println("No available define for that status code");
+      }
+      getWifiInfo();
+    }
+    else if (command == (uint8_t)'f')
+    {
+      OTAModeOn();
+    }
+    else if (command == (uint8_t)'a')
+    {
+      getAdvancedInfo();
+    }
+    else if (command == (uint8_t)'h')
+    {
+      // Print help aboud available commands
+      Serial.println("-------------------------------------------------------------------------");
+      Serial.println("Available commands, send the character of the command you wish to execute");
+      Serial.println("'r' - restart the ESP");
+      Serial.println("'m' - print system_get_free_heap_size() (remaining SRAM)");
+      Serial.println("'t' - print the time of the ESP");
+      Serial.println("'u' - print the generated URLs, also these are the active settings for the strips");
+      Serial.println("'o' - set all strips to OFF effect");
+      Serial.println("'n' - print network info");
+      Serial.println("'f' - firmware upgrade, OTA mode on");
+      Serial.println("'a' - advanced info on ESP");
+      Serial.println("");
+    }
   }
   
   if(millis() > timer)
   {
-    // ------------------------------ BASIC HARTBEAT INFO
-    timeClient.update();
+    #ifdef NTP_UPDATE
+      timeClient.update();
+    #endif
     timer = millis() + HEARTBEAT_PERIOD;
     
+    // ------------------------------ BASIC HARTBEAT INFO
     #ifdef VERBOSE_MODE
-      Serial.print("Elapsed minutes since boot: ");
-      Serial.println(millis()/60000);
-      Serial.print("Time from NTP server: ");
-      Serial.println(timeClient.getFormattedTime());
+      getTimeInfo();
+      getMemoryInfo();
     #endif
-    
-    /* 
-    // All of a sudden started causing CPU crash after implementing EEPROM read on startup:/
-    Serial.print("Free bytes in RAM: ");
-    Serial.println(system_get_free_heap_size());
-    */
     
     // ------------------------------ WIFI RECONNECT IN CASE LOST CONNECTION
     if(WiFi.status() != WL_CONNECTED)
     {
-      while (WiFi.status() != WL_CONNECTED) {
+      Serial.println("WIFI CONNECTION LOST, RECONNECTING...");
+      WiFi.reconnect();
+      while (WiFi.status() != WL_CONNECTED)
+      {
         delay(1000);
         Serial.print(".");
       }
       Serial.println("");
-      Serial.print("Successfully reconnected to ");
-      Serial.println(WIFI_SSID);
-      Serial.print("IP address is ");
-      Serial.println(WiFi.localIP());
+      getWifiInfo();
     }
   }
+}
+
+// ------------------------------------------------------------------------------------------ MISC FUNCTIONS
+void OTAModeOn()
+{
+  server.send(200, "text/plain", "OTA MODE ON, waiting for fw for 30 sec. You can close this tab.");
+  Serial.println("OTA MODE ON, waiting for fw for 30 sec");
+  
+  uint32_t startTime = millis() + 30000;
+  while(startTime > millis())
+  {
+    ArduinoOTA.handle();
+    delay(100);
+  }
+  Serial.println("Failed to upgrade, restarting...");
+  delay(100);
+  ESP.restart();
+}
+
+void getMemoryInfo()
+{
+  uint32_t freeMemory;
+  uint16_t maxBlock;
+  uint8_t fragmentation;
+  
+  //freeMemory = ESP.getFreeHeap();
+  //maxBlock = ESP.getMaxFreeBlockSize();
+  //fragmentation = ESP.getHeapFragmentation();
+  
+  ESP.getHeapStats(&freeMemory, &maxBlock, &fragmentation);
+  Serial.println("ESP memory stats:");
+  Serial.print("  Free Heap Size: ");
+  Serial.println(freeMemory);
+  Serial.print("  Max Block Size: ");
+  Serial.println(maxBlock);
+  Serial.print("  Fragmentation%: ");
+  Serial.println(fragmentation);
+}
+
+void getTimeInfo()
+{
+  Serial.print("Ellapsed minutes since boot: ");
+  Serial.println(millis()/60000);
+  Serial.print("Time from NTP server: ");
+  Serial.println(timeClient.getFormattedTime());
+}
+
+void getWifiInfo()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.print("Connected to: ");
+    Serial.println(WIFI_SSID);
+    Serial.print("IP address is: ");
+    Serial.println(WiFi.localIP());
+  }
+  else
+  {
+    Serial.println("No info available, disconnected from WiFi");
+  }
+}
+
+void getAdvancedInfo()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    getWifiInfo();
+    uint8_t macAddr[6];
+    WiFi.macAddress(macAddr);
+    Serial.printf("MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+    Serial.print("Subnet mask: ");
+    Serial.println(WiFi.subnetMask());
+    Serial.print("Gateway IP: ");
+    Serial.println(WiFi.gatewayIP());
+    Serial.print("DHCP hostname: ");
+    Serial.println(WiFi.hostname());
+    Serial.print("RSSI[dB]: ");
+    Serial.println(WiFi.RSSI());
+      
+  }
+  else
+  {
+    Serial.println("No info available, disconnected from WiFi");
+  }
+  // TODO convert to volt
+  Serial.print("3V3 supply rail at: ");
+  Serial.println(ESP.getVcc());
+  Serial.print("Chip ID: ");
+  Serial.println(ESP.getChipId());
+  Serial.print("SDK version: ");
+  Serial.println(ESP.getSdkVersion());
+  Serial.print("Core version: ");
+  Serial.println(ESP.getCoreVersion());
+  Serial.print("Full version: ");
+  Serial.println(ESP.getFullVersion());
+  Serial.print("Boot version: ");
+  Serial.println(ESP.getBootVersion());
+  Serial.print("Boot mode: ");
+  Serial.println(ESP.getBootMode());
+  Serial.print("CPU frequency[MHz]: ");
+  Serial.println(ESP.getCpuFreqMHz());
+  Serial.print("Flash chip ID: ");
+  Serial.println(ESP.getFlashChipId());
+  Serial.print("Flash chip vendor ID: ");
+  Serial.println(ESP.getFlashChipVendorId());
+  Serial.print("Flash chip real size[byte]: ");
+  Serial.println(ESP.getFlashChipRealSize());
+  Serial.print("Flash chip size[byte]: ");
+  Serial.println(ESP.getFlashChipSize());
+  Serial.print("Flash chip speed[HZ]: ");
+  Serial.println(ESP.getFlashChipSpeed());
+  Serial.print("Flash chip mode: ");
+  Serial.println(ESP.getFlashChipMode());
+  Serial.print("Flash chip size by chip ID[byte]: ");
+  Serial.println(ESP.getFlashChipSizeByChipId());
+  Serial.print("Sketch size[byte]: ");
+  Serial.println(ESP.getSketchSize());
+  Serial.print("Sketch MD5: ");
+  Serial.println(ESP.getSketchMD5());
+  Serial.print("Free sketch space[byte]: ");
+  Serial.println(ESP.getFreeSketchSpace());
+
 }
